9864e15b38127ac9797b411ca4e46ad5bd282195
mohamed-SaberA2 <ms1464684@gmail.com>
2026-06-10 08:22:52 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,21 +1,33 @@
 class AuthManager {
-    constructor() {
-        this.token = localStorage.getItem('authToken');
-        this.user = null;
+  constructor() {
+    this.token = localStorage.getItem('authToken');
+  }
+
+  setToken(token) {
+    this.token = token;
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken() {
+    return this.token;
+  }
+
+  clearToken() {
+    this.token = null;
+    localStorage.removeItem('authToken');
+  }
+
+  isAuthenticated() {
+    return !!this.token;
+  }
+
+  logout() {
+    this.clearToken();
+    if (window.app) {
+      window.app.showPage('login');
     }
-    setToken(token) {
-        this.token = token;
-        localStorage.setItem('authToken', token);
-    }
-    getToken() {
-        return this.token;
-    }
-    clearToken() {
-        this.token = null;
-        localStorage.removeItem('authToken');
-    }
-    isAuthenticated() {
-        return !!this.token;
-    }
+    history.pushState({}, "", "/");
+  }
 }
+
 window.authManager = new AuthManager();
